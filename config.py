9b4626666e099db88571006578113ec5d4e72c1c
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2022-12-24 00:36:00 +0330

    disable default proxy

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ name: str = 'alisk'
 api_id: int = 7313104
 api_hash: str = 'f31ec6e9e3729a286d1cffdb5c2d9a68'
 
-use_proxy = True
+use_proxy = False
 proxy = dict(scheme="socks5", hostname="127.0.0.1", port=10808)
 
 auto_seen = False
